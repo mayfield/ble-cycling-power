d52892b3f37766892f9f47ecce540d91a4b6b127
Justin Mayfield <tooker@gmail.com>
2023-12-19 21:49:51 -0700

    Add nonfunctional dev info serv

---
+++ device-information-service.js
@@ -0,0 +1,40 @@
+const bleno = require('@abandonware/bleno');
+
+
+class SerialNumberCharacteristic extends bleno.Characteristic {
+    constructor() {
+        super({
+            uuid: '2a25',
+            properties: ['read'],
+            value: Buffer.from('867-5309'),
+            descriptors: [new bleno.Descriptor({uuid: '2901', value: '867-5309'})]
+        });
+    }
+}
+
+
+class ManufacturerCharacteristic extends bleno.Characteristic {
+    constructor() {
+        super({
+            uuid: '2a29',
+            properties: ['read'],
+            value: Buffer.from('Sauce for BLE'),
+            descriptors: [new bleno.Descriptor({uuid: '2901', value: 'foobar'})]
+        });
+    }
+}
+
+
+class DeviceInformationService extends bleno.PrimaryService {
+    constructor() {
+        super({
+            uuid: '180a',
+            characteristics: [
+                new SerialNumberCharacteristic(),
+                new ManufacturerCharacteristic(),
+            ],
+        });
+    }
+}
+
+module.exports = DeviceInformationService;
